f38570c45fc996d7ed102724c45c0fdf1ddd5dff
N-Githu <u24607917@tuks.co.za>
2026-09-02 23:22:29 +0200

    Deliverable 1: Initial setup with Vite + React, React Router, Express, and folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SplashPage from './pages/SplashPage';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import PostPage from './pages/PostPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SplashPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile/:id" element={<ProfilePage />} />
+        <Route path="/post/:id" element={<PostPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
